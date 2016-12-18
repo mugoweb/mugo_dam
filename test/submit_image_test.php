b8a7f89d015f5f8ca3423c0e4bf42e9de324bc16
Philipp Kamps <pkamps@mugo.ca>
2016-12-18 19:58:59 +0100

    Test for rename service added

--- test/submit_image_test.php
+++ test/submit_image_test.php
@@ -1,25 +1,26 @@
 <?php
 
-$endpoint = 'http://imageserver.example.com/upload';
+$server = 'http://www.example.com';
+$key = 'topsecret';
 $filename = 'download.png';
 
 $data = array(
-	'files[0]'      => '@' . $filename,
-	'creationtime'  => 1461769869,
+        'files[0]'      => '@' . $filename,
+        'creationtime'  => 1461769869,
 );
 
 $headers = array(
-	'Content-Disposition: attachment; filename='. rawurlencode( $filename ) .';',
+        'Content-Disposition: attachment; filename='. rawurlencode( $filename ) .';',
 );
 
 $access_details = array(
-	'repository'	=> 'mugo_test',
-	'apikey'		=> 'topsecret',
+        'repository'    => 'client_test',
+        'apikey'                => $key,
 );
 
 $data += $access_details;
 
-$ch = curl_init( $endpoint );
+$ch = curl_init( $server . '/upload' );
 
 curl_setopt( $ch, CURLOPT_POST, true );
 curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
@@ -28,9 +29,35 @@ curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
 
 if( !empty( $headers ) )
 {
-	curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
+        curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
 }
 
-$jsonText = curl_exec( $ch );
+$jsonResponse = curl_exec( $ch );
+$responseData = json_decode( $jsonResponse );
 
-var_dump( $jsonText );
+echo 'Result from upload:' . "\n";
+print_r( $responseData );
+
+$uploadFile = $responseData->files[0]->urlPath;
+
+if( $uploadFile )
+{
+        $data = array(
+                'source' => $uploadFile,
+                'target_name' => 'renamed.png'
+        );
+
+        $data += $access_details;
+
+        $ch = curl_init( $server . '/rename' );
+        curl_setopt( $ch, CURLOPT_POST, true );
+        curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
+        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+
+        $jsonResponse = curl_exec( $ch );
+        $responseData = json_decode( $jsonResponse );
+
+        echo 'Result of rename:' . "\n";
+        var_dump( $responseData );
+}
