291835beac718acf095c94e719fe2b532e96b0d7
pkamps <pkamps@mugo.ca>
2023-04-11 12:21:12 +0200

    Merged with CSM changes

--- test/submit_image_test.php
+++ test/submit_image_test.php
@@ -5,20 +5,15 @@ $key = 'topsecret';
 $filename = 'download.png';
 
 $data = array(
-        'files[0]'      => '@' . $filename,
-        'creationtime'  => 1461769869,
+	'files[0]' => new CurlFile( 'download.png', 'image/png', 'download.png'),
+	'creationtime' => 1461769869,
+	'repository' => $repo,
+	'apikey' => $key,
 );
 
 $headers = array(
-        'Content-Disposition: attachment; filename='. rawurlencode( $filename ) .';',
+	'Content-Disposition: attachment; filename='. rawurlencode( 'download.png' ) .';',
 );
-
-$access_details = array(
-        'repository'    => 'client_test',
-        'apikey'                => $key,
-);
-
-$data += $access_details;
 
 $ch = curl_init( $server . '/upload' );
 
@@ -29,35 +24,43 @@ curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
 
 if( !empty( $headers ) )
 {
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
+	curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
 }
 
 $jsonResponse = curl_exec( $ch );
 $responseData = json_decode( $jsonResponse );
 
-echo 'Result from upload:' . "\n";
-print_r( $responseData );
+if( $responseData )
+{
+	echo 'Result from upload:' . "\n";
+	print_r( $responseData );
+}
+else
+{
+	echo 'No proper json response:' . "\n";
+	var_dump( $jsonResponse );
+}
 
 $uploadFile = $responseData->files[0]->urlPath;
 
 if( $uploadFile )
 {
-        $data = array(
-                'source' => $uploadFile,
-                'target_name' => 'renamed.png'
-        );
+	$data = array(
+		'source' => $uploadFile,
+		'target_name' => 'renamed.png',
+		'repository' => $repo,
+		'apikey' => $key,
+	);
 
-        $data += $access_details;
+	$ch = curl_init( $server . '/rename' );
+	curl_setopt( $ch, CURLOPT_POST, true );
+	curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
+	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+	curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
 
-        $ch = curl_init( $server . '/rename' );
-        curl_setopt( $ch, CURLOPT_POST, true );
-        curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+	$jsonResponse = curl_exec( $ch );
+	$responseData = json_decode( $jsonResponse );
 
-        $jsonResponse = curl_exec( $ch );
-        $responseData = json_decode( $jsonResponse );
-
-        echo 'Result of rename:' . "\n";
-        var_dump( $responseData );
+	echo 'Result of rename:' . "\n";
+	var_dump( $responseData );
 }
